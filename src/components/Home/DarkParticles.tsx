0ebf81140a82e065dd336882fc0a362ee1ae0ccb
luis jimenez <luisjc140992@gmail.com>
2021-09-26 16:30:44 -0500

    Particles, antd added

---
+++ src/components/Home/DarkParticles.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import Particles from "react-tsparticles";
+
+const DarkParticles = () => {
+  return (
+    <Particles
+      id="tsparticles"
+      className="tsparticles"
+      options={{
+        background: {
+          color: {
+            value: "#090b10",
+          },
+        },
+        fpsLimit: 60,
+        interactivity: {
+          events: {
+            onhover: {
+              enable: true,
+              mode: "bubble",
+            },
+            onclick: {
+              enable: true,
+              mode: "repulse",
+            },
+          },
+          modes: {
+            bubble: {
+              distance: 250,
+              duration: 2,
+              size: 0,
+              opacity: 0,
+            },
+            repulse: {
+              distance: 400,
+              duration: 4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "top",
+            enable: true,
+            outMode: "out",
+            random: true,
+            speed: 0.5,
+          },
+          number: {
+            value: 170,
+            density: {
+              enable: false,
+            },
+          },
+          size: {
+            random: true,
+            value: 2,
+            anim: {
+              speed: 4,
+              size_min: 0.3,
+            },
+          },
+          line_linked: {
+            enable: false,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+};
+
+export default DarkParticles;
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DarkParticles from "./DarkParticles";
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <DarkParticles />
+    </div>
+  );
+};
+
+export default Home;
