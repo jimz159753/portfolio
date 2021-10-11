dfcf1481aa4639312f1b79aa689e17b083bd4515
luis jimenez <luisjc140992@gmail.com>
2021-10-11 13:52:01 -0500

    Refactoring & contact functionality

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -5,12 +5,14 @@ import { Form, Input, Button, Row, Col } from "antd";
 const { TextArea } = Input;
 
 const Contact = () => {
+  const [formRef] = Form.useForm();
   return (
     <div id="contact" className="contact-container">
       <div className="wrapper">
         <h1>Contact me</h1>
         <p>I am available for full time, part time and freelance work.</p>
-        <Form name="basic" className="form">
+        <Form name="basic" className="form" form={formRef} onFinish={() => formRef.resetFields()}
+        >
           <Row gutter={[32, 8]} align="middle" justify="center">
             <Col md={12} xs={24}>
               <Form.Item name="username">
@@ -18,21 +20,21 @@ const Contact = () => {
               </Form.Item>
             </Col>
             <Col md={12} xs={24}>
-              <Form.Item name="username">
+              <Form.Item name="email">
                 <Input placeholder="Your email*" />
               </Form.Item>
             </Col>
           </Row>
           <Row>
             <Col span={24}>
-              <Form.Item name="username">
+              <Form.Item name="subject">
                 <Input placeholder="Write a Subject" />
               </Form.Item>
             </Col>
           </Row>
           <Row>
             <Col span={24}>
-              <Form.Item name="username">
+              <Form.Item name="message">
                 <TextArea placeholder="Your Message" />
               </Form.Item>
             </Col>
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,29 +4,40 @@ import './Projects.scss';
 import inicont from '../../assets/images/inicont.png';
 import medical_record from '../../assets/images/medical_record.png';
 
+const ok = [
+  {
+    img: inicont,
+    techs: ['ReactJs', 'Redux', 'MongoDB']
+  },
+  {
+    img: medical_record,
+    techs: ['ReactJs', 'Flask', 'MongoDB']
+  }
+]
+
 const Projects = () => (
   <div id="projects" className="projects-container">
     <h1>My latest projects</h1>
     <p>Here are a few projects I've worked on recently.</p>
     <div className="projects">
-      <Image
-        data-aos="zoom-in-up"
-        data-aos-duration="1500"
-        src={inicont}
-
-      />
-
-      <Image
-        data-aos="zoom-in-up"
-        data-aos-duration="1500"
-        src={medical_record}
-      />
-
-      <Image
-        data-aos="zoom-in-up"
-        data-aos-duration="1500"
-        src={inicont}
-      />
+      {
+        ok.map((el, i) => (
+          <Image
+            key={i}
+            data-aos="zoom-in-up"
+            data-aos-duration="1500"
+            src={el.img}
+            preview={{
+              mask: <div className='mask-container'>
+                <div className='mask'>
+                  <p>preview</p>
+                </div>
+                <p>{el.techs.join(', ')}</p>
+              </div>
+            }}
+          />
+        ))
+      }
     </div>
   </div>
 );
